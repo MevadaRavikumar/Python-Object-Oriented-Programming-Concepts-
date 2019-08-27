a35636fe6391f4d359a326b578b625d588feeeec
MevadaRavikumar <mevada.ravikumar@gmail.com>
2019-08-27 14:14:19 +0200

    class in class got explained with simple example

--- OOP_Python_01.py
+++ OOP_Python_01.py
@@ -27,6 +27,7 @@ class Student:
         self.m1 = m1
         self.m2 = m2
         self.m3 = m3
+        self.lap = self.Laptop()  #object of Laptop class
 
     # instance method: because it depends on object or instance
     def avg (self):
@@ -36,8 +37,10 @@ class Student:
         return self.m1
     # mutatores method (setters)
     def set_m1 (self, value):
-        self.m1 = value
-  
+        self.m1 = value 
+    def show (self):
+        print (self.m1, self.m2, self.m3)
+
     # class method
     @classmethod
     def getschoolname (cls):
@@ -45,12 +48,32 @@ class Student:
     @staticmethod    
     def info ():
         print ("This is static method!")
+    
+    class Laptop: 
+         def __init__(self):
+             self.brand = 'Acer'
+             self.cpu = 'i5'
+             self.ram = '8GB'
+         def show (self):
+              print (self.brand, self.cpu, self.ram)
         
 s1 = Student(60, 70, 78)
 s2 = Student(90, 92, 65)
+
+# creating object of lap
+lap1 = s1.lap
+lap2 = s2.lap
+
+lap3 = Student.Laptop()
 
 print (s1.avg())
 print (s2.avg())
 print (s1.getschoolname())
 print (Student.getschoolname())
 print (Student.info())
+print (s1.lap.brand)
+print (s2.lap.cpu)
+print (lap1.cpu)
+print (lap2.cpu)
+print (lap3.show())
+print (s1.show())
