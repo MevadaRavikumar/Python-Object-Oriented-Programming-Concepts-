beb290e87ccb1f5b671d9b15033e4e0d2b281792
MevadaRavikumar <mevada.ravikumar@gmail.com>
2019-08-28 11:35:53 +0200

    multi level in inheritance got explained with simple example

--- OOP_Python_01.py
+++ OOP_Python_01.py
@@ -29,6 +29,15 @@ class B(A):
         print ("This is method 3")
     def method4(self):
         print ("This is method 4")
+
+# Class C is sub sub class
+# Here class C is inheriting class B and class B is inheriting class A 
+
+class C(B):
+    def method5(self):
+        print ("This is method 5")
+    def method6(self):
+        print ("This is method 6")
         
 # Here class A is only having method 1&2 because class A is not inheriting class B
 a = A()
@@ -42,3 +51,13 @@ b.method1()
 b.method2()
 b.method3()
 b.method4()
+
+# class C will have all the methods of Class A and B and also its own
+# This is called multi level of inheritance
+c = C()
+c.method1()
+c.method2()
+c.method3()
+c.method4()
+c.method5()
+c.method6()
