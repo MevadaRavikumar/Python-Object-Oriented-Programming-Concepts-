bb00cc7b4529aacbe8d09c89ba0a18e7844aed41
MevadaRavikumar <mevada.ravikumar@gmail.com>
2019-09-01 14:41:18 +0200

    Duck typing got explained

--- OOP_Python_01.py
+++ OOP_Python_01.py
@@ -7,7 +7,9 @@ Created on Tue Aug 27 09:09:48 2019
 
 
 """
-Types of Methods
+Types of Methods and Polymorphism
+
+*Types of Methods
 1. Instance Method - works with instance and instacne variables
 2. Class Method - works with class variables
                 
@@ -18,6 +20,13 @@ Types of Methods
 3. Static Method - method which has nothing to do with instance or class variables, useful if you want to perform some 
                     operation which has to do something with other class object
          
+*Polymorphism
+- Ploymorphism means many forms, here objects can have multiple form 
+- four ways of implimenting polymorphism 
+1. Duck Typing
+2. Operator Overloading 
+3. Method Overloading
+4. Method Overriding
 """
 class Student:
     
@@ -77,3 +86,30 @@ print (lap1.cpu)
 print (lap2.cpu)
 print (lap3.show())
 print (s1.show())
+
+# Duck Typing 
+# Duck typing is used when you have same kind og method in different classes and you want to call that method from those classes depending on the situation 
+# There are two classes called acer and lenovo and both are having the same features method
+class acer:
+    def features(self):
+        print ("i5 processor")
+        print ("8GB RAM")
+        print ("1TB HD")
+
+class lenovo: 
+    def features(self):
+        print ("i7 processor")
+        print ("16GB RAM")
+        print ("512GB SSD HD")
+
+class Laptop:  
+    def show_feature (self, company):
+        company.features() 
+
+# Here the type of company object can be change from lenovo to acer or vise versa
+# This is called duck typing 
+company = lenovo()
+
+lap1 = Laptop()
+lap1.show_feature(company)
+ 
