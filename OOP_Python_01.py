0690cb211413b1bde1545a73d0597f96385beef4
MevadaRavikumar <mevada.ravikumar@gmail.com>
2019-09-01 15:27:12 +0200

    operator overloading got explained

--- OOP_Python_01.py
+++ OOP_Python_01.py
@@ -88,7 +88,7 @@ print (lap3.show())
 print (s1.show())
 
 # Duck Typing 
-# Duck typing is used when you have same kind og method in different classes and you want to call that method from those classes depending on the situation 
+# Duck typing is used when you have same kind og method in different classes and you want to call that method from those classes depending on the situation  
 # There are two classes called acer and lenovo and both are having the same features method
 class acer:
     def features(self):
@@ -113,3 +113,40 @@ company = lenovo()
 lap1 = Laptop()
 lap1.show_feature(company)
  
+#Operator overloading
+"""
+Ex: 
+    
+5+6 == int.__add__(5,6)
+Here 5 and 6 are the integer operands and + is operator, in the same way we can do addition of integers, floats, strings 
+what if you want to do 5 + 'Hello World' - it will give you an error of unsupported operand types, because there is no inbuilt method to add integers and strings
+so all this kind of methods (int.__add__(), str.__add__()) are predefined, it is called as Synthetic Sugar
+"""
+
+class Student:
+    
+    def __init__ (self, m1, m2):
+        self.m1 = m1
+        self.m2 = m2
+    
+    # method for add operator overloading 
+    def __add__ (self, other):
+        m1 = self.m1 + other.m1
+        m2 = self.m2 + other.m2
+        s3 = Student (m1, m2)
+        return s3
+    
+        
+s1 = Student (56,78)
+s2 = Student (34,98)
+
+"""
+if you want to add that two students marks 
+Ex: s3 = s1 + s2
+it will give you an error of unsupported operand types, beause there is no add method in the Student calss
+but it is possible to define the method, which is called operator overloading 
+"""
+# Now it is possible to add , in the same way you can define other method like multiplication, greater, substraction, etc
+s3 = s1 +s2
+print (s3.m1)
+print (s3.m2)
